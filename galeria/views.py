a0eeff109bf60eca66cb61dc48ff84ee9eb37379
souza398 <souzaxsz8@gmail.com>
2026-06-18 18:50:33 -0300

    projeto-concluido

--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'galeria/index.html')
+
+def imagem (request):
+    return render(request, 'galeria/imagem.html')
