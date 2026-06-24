e9953b5052d103207d6f324167fb79938bd44929
souza398 <souzaxsz8@gmail.com>
2026-06-24 18:18:05 -0300

    add-admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
+
 
 def index(request):
+    fotografias = Fotografia.objects.filter(publicada=True)
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-    dados = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "webbtelescope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"}
-    }
-
-    return render(request, 'galeria/index.html', {"cards": dados})
-
-def imagem (request):
-    return render(request, 'galeria/imagem.html')
+def imagem (request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
